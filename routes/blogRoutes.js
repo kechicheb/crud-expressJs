fde0a4f87c5a996578e88d25c3fdca5d82025494
kechicheb <93807375+kechicheb@users.noreply.github.com>
2022-11-27 21:13:11 +0100

    added routes

---
+++ routes/blogRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const blogController = require("../controllers/blogController");
+
+const router = express.Router();
+
+router.get("/", blogController.blog_index);
+router.get("/create", blogController.blog_create_get);
+router.post("/", blogController.blog_create_post);
+router.put("/edit/:id", blogController.blog_update);
+router.get("/edit/:id", blogController.blog_edit);
+router.get("/:id", blogController.blog_details);
+router.delete("/:id", blogController.blog_delete);
+
+module.exports = router;
